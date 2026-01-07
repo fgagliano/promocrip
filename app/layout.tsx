eae5f862a5ebcd3d1e7ddc484e5ca8f12d945b9b
fgagliano <fabiogagliano@terra.com.br>
2026-01-06 23:08:36 -0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,9 @@
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="pt-BR">
+      <body style={{ margin: 0, fontFamily: "system-ui, Arial" }}>
+        {children}
+      </body>
+    </html>
+  );
+}
