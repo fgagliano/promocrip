6767c6ecfa2d86ce24ad078864184597ec609657
fgagliano <fabiogagliano@terra.com.br>
2026-01-06 23:29:08 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
-      <body style={{ margin: 0, fontFamily: "system-ui, Arial" }}>
-        {children}
-      </body>
+      <body style={{ margin: 0, fontFamily: "system-ui, Arial" }}>{children}</body>
     </html>
   );
 }
- 
