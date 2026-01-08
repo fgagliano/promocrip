bf00c5aa911df34895a58e9efd2649350789aa0d
fgagliano <fabiogagliano@terra.com.br>
2026-01-08 02:05:49 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -138,6 +138,7 @@ export default function Page() {
   });
 
 const [rendaFixa, setRendaFixa] = useState<RendaFixa[]>([]);
+const [aportes, setAportes] = useState<Aporte[]>([]);
 
   
   async function carregarTudo() {
